f18fa1bfb7632664251fb3e5fbff3ea238d6dfc6
Susanna Kosonen <susannakosonen@Susannas-MacBook-Pro.local>
2018-08-12 15:05:29 -0400

    Done.

--- src/components/CocktailDisplay.js
+++ src/components/CocktailDisplay.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const CocktailDisplay = (props) => {
+
   return (
     <div id="cocktail-display">
-      <h1>{/* Cocktail Name */}</h1>
-      <h3>{/* Cocktail Description */}</h3>
-      <p>{/* Cocktail Instructions */}</p>
+      <h1>{props.cocktail ? props.cocktail.name : null}</h1>
+      <h3>{props.cocktail ? props.cocktail.description : null}</h3>
+      <p>{props.cocktail ? props.cocktail.instructions : null}</p>
+      <h3>Ingredients</h3>
+      <ul>{props.proportions.map(item => <li key={item.id}><strong>{item.amount}</strong> {item.ingredient_name}</li>)}</ul>
     </div>
   )
 }
--- src/components/CocktailsContainer.js
+++ src/components/CocktailsContainer.js
@@ -4,11 +4,56 @@ import CocktailDisplay from './CocktailDisplay'
 import Form from './Form'
 
 class CocktailsContainer extends Component {
+  constructor() {
+    super()
+    //it would be optimal to not have cocktails in state, since they won't change!
+    this.state = {
+      cocktails: [],
+      currentCocktail: null,
+      //do i need this here?
+      currentProportionsAndIngredients: [],
+    }
+  }
+  //fetch all cocktails when document created
+  componentDidMount() {
+    this.fetchCocktails()
+  }
+
+  fetchCocktails = () => {
+    fetch("http://localhost:3000/api/v1/cocktails")
+    .then(r => r.json()).then(results => {
+      this.setState({
+        cocktails: results
+      })
+    })
+  }
+
+  setCurrentCocktail = (cocktailObj) => {
+    this.setState({
+      currentCocktail: cocktailObj
+    }, this.getIngredientsForCurrentCocktail)
+  }
+
+  getIngredientsForCurrentCocktail = () => {
+    //fetch the data for current cocktail, set current ingredients to be that!
+    fetch(`http://localhost:3000/api/v1/cocktails/${this.state.currentCocktail.id}`).then(r => r.json())
+    .then(cocktail => {
+      this.setState({
+        currentProportionsAndIngredients: cocktail.proportions
+      })
+    })
+  }
 
   render(){
     return (
       <div className="container">
-      
+        <CocktailsList cocktails={this.state.cocktails} handleCocktailDisplay={this.setCurrentCocktail}/>
+
+        <CocktailDisplay cocktail={this.state.currentCocktail} proportions={this.state.currentProportionsAndIngredients}/>
+        
+        <Form 
+        updateCocktailList={this.fetchCocktails}
+        displayNewCocktail={this.setCurrentCocktail}/>
       </div>
     )
   }
